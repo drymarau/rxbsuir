33fc48658e5c8ef8fe125b387c4b921754697b52
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-07-07 21:43:25 +0300

    Add blank activity with a toolbar

---
+++ app/src/main/java/by/toggi/rxbsuir/ScheduleActivity.java
@@ -0,0 +1,25 @@
+package by.toggi.rxbsuir;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar) Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+
+        ButterKnife.bind(this);
+
+        getDelegate().setSupportActionBar(mToolbar);
+    }
+
+}
