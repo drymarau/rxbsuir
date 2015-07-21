1bd31df9f377416dd974e01dab42de36a7cd2f7e
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-07-21 15:20:10 +0300

    Add lessonentry to contract

--- app/src/main/java/by/toggi/rxbsuir/db/RxBsuirContract.java
+++ app/src/main/java/by/toggi/rxbsuir/db/RxBsuirContract.java
@@ -4,7 +4,7 @@ import android.provider.BaseColumns;
 
 public class RxBsuirContract {
 
-    public static class StudentGroupEntry implements BaseColumns {
+    public static class StudentGroupEntry {
 
         public static final String TABLE_NAME = "students_groups";
 
@@ -13,6 +13,23 @@ public class RxBsuirContract {
         public static final String COL_COURSE = "course";
         public static final String COL_FACULTY_ID = "faculty_id";
         public static final String COL_SPECIALITY_DEPARTMENT_EDUCATION_FORM_ID = "speciality_department_education_form_id";
+
+    }
+
+    public static class LessonEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "lessons";
+
+        public static final String COL_WEEKDAY = "weekday";
+        public static final String COL_WEEK_NUMBER_LIST = "week_number_list";
+        public static final String COL_SUBJECT = "subject";
+        public static final String COL_STUDENT_GROUP_LIST = "student_group_list";
+        public static final String COL_NUM_SUBGROUP = "num_subgroup";
+        public static final String COL_NOTE = "note";
+        public static final String COL_LESSON_TIME = "lesson_time";
+        public static final String COL_LESSON_TYPE = "lesson_type";
+        public static final String COL_EMPLOYEE_LIST = "employee_list";
+        public static final String COL_AUDITORY_LIST = "auditory_list";
 
     }
 
