06101c073aef5af4214c90dfe24dd6e3c671f399
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-07-22 18:10:55 +0300

    Add one line view to lesson adapter

--- app/src/main/java/by/toggi/rxbsuir/adapter/LessonAdapter.java
+++ app/src/main/java/by/toggi/rxbsuir/adapter/LessonAdapter.java
@@ -16,10 +16,12 @@ import by.toggi.rxbsuir.db.model.Lesson;
 
 public class LessonAdapter extends RecyclerView.Adapter<LessonAdapter.ViewHolder> {
 
-    public static final int VIEW_TYPE_LESSON_TWO_LINE = 0;
-    public static final int VIEW_TYPE_LESSON_TWO_LINE_WITH_WEEKDAY = 1;
-    public static final int VIEW_TYPE_LESSON_THREE_LINE = 2;
-    public static final int VIEW_TYPE_LESSON_THREE_LINE_WITH_WEEKDAY = 3;
+    public static final int VIEW_TYPE_LESSON_ONE_LINE = 0;
+    public static final int VIEW_TYPE_LESSON_ONE_LINE_WITH_WEEKDAY = 1;
+    public static final int VIEW_TYPE_LESSON_TWO_LINE = 2;
+    public static final int VIEW_TYPE_LESSON_TWO_LINE_WITH_WEEKDAY = 3;
+    public static final int VIEW_TYPE_LESSON_THREE_LINE = 4;
+    public static final int VIEW_TYPE_LESSON_THREE_LINE_WITH_WEEKDAY = 5;
     private List<Lesson> mLessonList;
 
     public LessonAdapter(List<Lesson> lessonList) {
@@ -36,16 +38,25 @@ public class LessonAdapter extends RecyclerView.Adapter<LessonAdapter.ViewHolder
         Lesson lesson = mLessonList.get(position);
 
         if (position == 0) {
+            if (lesson.getPrettyAuditoryList().isEmpty()) {
+                return VIEW_TYPE_LESSON_ONE_LINE_WITH_WEEKDAY;
+            }
             return lesson.getPrettyEmployeeList().isEmpty()
                     ? VIEW_TYPE_LESSON_TWO_LINE_WITH_WEEKDAY
                     : VIEW_TYPE_LESSON_THREE_LINE_WITH_WEEKDAY;
         }
 
         if (lesson.getWeekday().equals(mLessonList.get(position - 1).getWeekday())) {
+            if (lesson.getPrettyAuditoryList().isEmpty()) {
+                return VIEW_TYPE_LESSON_ONE_LINE;
+            }
             return lesson.getPrettyEmployeeList().isEmpty()
                     ? VIEW_TYPE_LESSON_TWO_LINE
                     : VIEW_TYPE_LESSON_THREE_LINE;
         } else {
+            if (lesson.getPrettyAuditoryList().isEmpty()) {
+                return VIEW_TYPE_LESSON_ONE_LINE_WITH_WEEKDAY;
+            }
             return lesson.getPrettyEmployeeList().isEmpty()
                     ? VIEW_TYPE_LESSON_TWO_LINE_WITH_WEEKDAY
                     : VIEW_TYPE_LESSON_THREE_LINE_WITH_WEEKDAY;
@@ -56,6 +67,14 @@ public class LessonAdapter extends RecyclerView.Adapter<LessonAdapter.ViewHolder
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         View view;
         switch (viewType) {
+            case VIEW_TYPE_LESSON_ONE_LINE:
+                view = LayoutInflater.from(viewGroup.getContext())
+                        .inflate(R.layout.list_item_lesson_one_line, viewGroup, false);
+                break;
+            case VIEW_TYPE_LESSON_ONE_LINE_WITH_WEEKDAY:
+                view = LayoutInflater.from(viewGroup.getContext())
+                        .inflate(R.layout.list_item_lesson_one_line_with_weekday, viewGroup, false);
+                break;
             case VIEW_TYPE_LESSON_TWO_LINE:
                 view = LayoutInflater.from(viewGroup.getContext())
                         .inflate(R.layout.list_item_lesson_two_line, viewGroup, false);
@@ -83,8 +102,10 @@ public class LessonAdapter extends RecyclerView.Adapter<LessonAdapter.ViewHolder
         Lesson lesson = mLessonList.get(position);
         viewHolder.lessonType.setText(lesson.getLessonType());
         viewHolder.lessonSubjectSubgroup.setText(lesson.getSubjectWithSubgroup());
-        viewHolder.lessonClass.setText(lesson.getPrettyAuditoryList());
         viewHolder.lessonTime.setText(lesson.getPrettyLessonTime());
+        if (viewHolder.lessonClass != null) {
+            viewHolder.lessonClass.setText(lesson.getPrettyAuditoryList());
+        }
         if (viewHolder.lessonWeekday != null) {
             viewHolder.lessonWeekday.setText(lesson.getWeekday());
         }
@@ -102,11 +123,11 @@ public class LessonAdapter extends RecyclerView.Adapter<LessonAdapter.ViewHolder
 
         @Bind(R.id.lesson_type) TextView lessonType;
         @Bind(R.id.lesson_subject_subgroup) TextView lessonSubjectSubgroup;
-        @Bind(R.id.lesson_class) TextView lessonClass;
         @Bind(R.id.lesson_time) TextView lessonTime;
 
         @Nullable @Bind(R.id.lesson_weekday) TextView lessonWeekday;
         @Nullable @Bind(R.id.lesson_employee) TextView lessonEmployee;
+        @Nullable @Bind(R.id.lesson_class) TextView lessonClass;
 
         public ViewHolder(View itemView) {
             super(itemView);
