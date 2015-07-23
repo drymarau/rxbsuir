ef54cf31cb3c3a2e59d9ca13298be3246756978f
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-07-23 13:59:01 +0300

    Annotate constructor with @Inject, add validation

--- app/src/main/java/by/toggi/rxbsuir/mvp/presenter/AddDialogPresenter.java
+++ app/src/main/java/by/toggi/rxbsuir/mvp/presenter/AddDialogPresenter.java
@@ -1,11 +1,26 @@
 package by.toggi.rxbsuir.mvp.presenter;
 
+import com.pushtorefresh.storio.sqlite.StorIOSQLite;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
 import by.toggi.rxbsuir.mvp.Presenter;
 import by.toggi.rxbsuir.mvp.view.AddDialogView;
+import by.toggi.rxbsuir.rest.BsuirService;
 
 public class AddDialogPresenter implements Presenter<AddDialogView> {
 
-    public AddDialogPresenter() {
+    private List<String> mGroupNumberList;
+    private AddDialogView mAddDialogView;
+    private BsuirService mService;
+    private StorIOSQLite mStorIOSQLite;
+
+    @Inject
+    public AddDialogPresenter(BsuirService service, StorIOSQLite storIOSQLite) {
+        mService = service;
+        mStorIOSQLite = storIOSQLite;
     }
 
     @Override
@@ -15,16 +30,30 @@ public class AddDialogPresenter implements Presenter<AddDialogView> {
 
     @Override
     public void onDestroy() {
-
+        detachView();
     }
 
     @Override
     public void attachView(AddDialogView addDialogView) {
-
+        mAddDialogView = addDialogView;
     }
 
     @Override
     public void detachView() {
+        mAddDialogView = null;
+    }
 
+    /**
+     * Validates group number.
+     *
+     * @param groupNumber the group number
+     * @return true is group number is valid, false otherwise
+     */
+    public boolean isValidGroupNumber(String groupNumber) {
+        if (mGroupNumberList != null) {
+            return mGroupNumberList.contains(groupNumber);
+        } else {
+            return true;
+        }
     }
 }
