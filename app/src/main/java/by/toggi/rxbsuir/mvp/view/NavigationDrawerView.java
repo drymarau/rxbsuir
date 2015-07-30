d327cd653e9c425d62bfd5f7a35a805fc2b20026
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-07-30 20:32:10 +0300

    Remove unused imports

--- app/src/main/java/by/toggi/rxbsuir/mvp/view/NavigationDrawerView.java
+++ app/src/main/java/by/toggi/rxbsuir/mvp/view/NavigationDrawerView.java
@@ -1,10 +1,8 @@
 package by.toggi.rxbsuir.mvp.view;
 
 import java.util.Map;
-import java.util.Set;
 
 import by.toggi.rxbsuir.mvp.View;
-import by.toggi.rxbsuir.rest.model.Employee;
 
 public interface NavigationDrawerView extends View {
 
