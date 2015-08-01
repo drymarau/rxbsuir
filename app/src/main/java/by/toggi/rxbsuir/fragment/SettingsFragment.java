85d440d743c78d08d5e10ce87f55d1e401ddc658
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-08-01 07:55:03 +0300

    Add build version info to settings

--- app/src/main/java/by/toggi/rxbsuir/fragment/SettingsFragment.java
+++ app/src/main/java/by/toggi/rxbsuir/fragment/SettingsFragment.java
@@ -10,6 +10,7 @@ import android.support.v4.content.IntentCompat;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import by.toggi.rxbsuir.BuildConfig;
 import by.toggi.rxbsuir.R;
 import by.toggi.rxbsuir.RxBsuirApplication;
 import by.toggi.rxbsuir.activity.ScheduleActivity;
@@ -31,6 +32,7 @@ public class SettingsFragment extends PreferenceFragment implements Preference.O
                         ? R.string.summary_pref_dark_theme_enabled
                         : R.string.summary_pref_dark_theme_disabled
         );
+        findPreference("build_version").setSummary(BuildConfig.VERSION_NAME);
     }
 
     @Override
