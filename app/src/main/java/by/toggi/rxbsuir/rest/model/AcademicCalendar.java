e22b4c4e014ca14101e07f470bd42911d4fda1e4
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-08-08 21:32:30 +0300

    Add academicCalendar model and service

---
+++ app/src/main/java/by/toggi/rxbsuir/rest/model/AcademicCalendar.java
@@ -0,0 +1,44 @@
+package by.toggi.rxbsuir.rest.model;
+
+/**
+ * The type Academic calendar.
+ */
+public class AcademicCalendar {
+
+    private int course;
+    private String startDate;
+    private String endDate;
+
+    /**
+     * Instantiates a new Academic calendar.
+     */
+    public AcademicCalendar() {
+    }
+
+    /**
+     * Gets course.
+     *
+     * @return the course
+     */
+    public int getCourse() {
+        return course;
+    }
+
+    /**
+     * Gets start date.
+     *
+     * @return the start date
+     */
+    public String getStartDate() {
+        return startDate;
+    }
+
+    /**
+     * Gets end date.
+     *
+     * @return the end date
+     */
+    public String getEndDate() {
+        return endDate;
+    }
+}
