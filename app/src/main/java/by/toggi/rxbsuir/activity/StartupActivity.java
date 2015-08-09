d4541bb78e039a77584dd8d827ea29ff655e9053
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-08-09 21:55:36 +0300

    Fix theme change crash

--- app/src/main/java/by/toggi/rxbsuir/activity/StartupActivity.java
+++ app/src/main/java/by/toggi/rxbsuir/activity/StartupActivity.java
@@ -9,7 +9,7 @@ public class StartupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, WeekScheduleActivity.class));
+        startActivity(new Intent(this, TermScheduleActivity.class));
         finish();
     }
 }
