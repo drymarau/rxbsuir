7431db37e317ba1cf0aea94d6ce12a3bb0611c9d
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-08-10 15:27:41 +0300

    Setup recyclerview

--- app/src/main/java/by/toggi/rxbsuir/activity/TermScheduleActivity.java
+++ app/src/main/java/by/toggi/rxbsuir/activity/TermScheduleActivity.java
@@ -1,11 +1,17 @@
 package by.toggi.rxbsuir.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+
+import javax.inject.Inject;
 
 import butterknife.Bind;
 import by.toggi.rxbsuir.R;
 import by.toggi.rxbsuir.RxBsuirApplication;
+import by.toggi.rxbsuir.SubheaderItemDecoration;
+import by.toggi.rxbsuir.adapter.LessonAdapter;
 import by.toggi.rxbsuir.component.DaggerTermScheduleActivityComponent;
 import by.toggi.rxbsuir.module.TermScheduleActivityModule;
 
@@ -13,9 +19,15 @@ public class TermScheduleActivity extends ScheduleActivity {
 
     @Bind(R.id.recycler_view) RecyclerView mRecyclerView;
 
+    @Inject LinearLayoutManager mLayoutManager;
+    @Inject LessonAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setupRecyclerView();
+
     }
 
     @Override
@@ -35,4 +47,14 @@ public class TermScheduleActivity extends ScheduleActivity {
     protected void showToday() {
 
     }
+
+    private void setupRecyclerView() {
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.addItemDecoration(new SubheaderItemDecoration(
+                LayoutInflater.from(this).inflate(R.layout.list_item_subheader, mRecyclerView, false),
+                getResources().getDimensionPixelSize(R.dimen.list_subheader_height)
+        ));
+    }
+
 }
