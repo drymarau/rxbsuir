34d29e9872b1b580002c9c41bc9560d6f79f0ad5
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-08-10 15:27:41 +0300

    Setup recyclerview

--- app/src/main/java/by/toggi/rxbsuir/module/TermScheduleActivityModule.java
+++ app/src/main/java/by/toggi/rxbsuir/module/TermScheduleActivityModule.java
@@ -1,7 +1,12 @@
 package by.toggi.rxbsuir.module;
 
+import android.support.v7.widget.LinearLayoutManager;
+
+import java.util.ArrayList;
+
 import by.toggi.rxbsuir.Activity;
 import by.toggi.rxbsuir.activity.TermScheduleActivity;
+import by.toggi.rxbsuir.adapter.LessonAdapter;
 import dagger.Module;
 import dagger.Provides;
 
@@ -18,6 +23,18 @@ public class TermScheduleActivityModule {
     @Activity
     TermScheduleActivity provideActivity() {
         return mActivity;
+    }
+
+    @Provides
+    @Activity
+    LinearLayoutManager provideLinearLayoutManager() {
+        return new LinearLayoutManager(mActivity);
+    }
+
+    @Provides
+    @Activity
+    LessonAdapter provideLessonAdapter() {
+        return new LessonAdapter(new ArrayList<>());
     }
 
 }
