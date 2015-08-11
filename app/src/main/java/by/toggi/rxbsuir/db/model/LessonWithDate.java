868f6d71b1a2d6c55c8216dc52edceab6dea800d
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-08-11 12:48:36 +0300

    Add getters for LocalDate and Lesson

--- app/src/main/java/by/toggi/rxbsuir/db/model/LessonWithDate.java
+++ app/src/main/java/by/toggi/rxbsuir/db/model/LessonWithDate.java
@@ -1,6 +1,7 @@
 package by.toggi.rxbsuir.db.model;
 
 import org.threeten.bp.LocalDate;
+import org.threeten.bp.format.DateTimeFormatter;
 
 public class LessonWithDate {
 
@@ -13,7 +14,7 @@ public class LessonWithDate {
     /**
      * New instance.
      *
-     * @param lesson the lesson
+     * @param lesson    the lesson
      * @param localDate the local date
      * @return the lesson with date
      */
@@ -22,6 +23,24 @@ public class LessonWithDate {
         lessonWithDate.lesson = lesson;
         lessonWithDate.localDate = localDate;
         return lessonWithDate;
+    }
+
+    /**
+     * Gets pretty date.
+     *
+     * @return the pretty date
+     */
+    public String getPrettyDate() {
+        return localDate.format(DateTimeFormatter.ISO_LOCAL_DATE);
+    }
+
+    /**
+     * Gets lesson.
+     *
+     * @return the lesson
+     */
+    public Lesson getLesson() {
+        return lesson;
     }
 
 }
