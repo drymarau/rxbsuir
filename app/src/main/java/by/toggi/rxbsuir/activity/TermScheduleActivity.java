6e78990b43909fb95c079b6e6c48ef3b5954b67e
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-08-11 13:39:25 +0300

    Implement group/employee switching, today scrolling

--- app/src/main/java/by/toggi/rxbsuir/activity/TermScheduleActivity.java
+++ app/src/main/java/by/toggi/rxbsuir/activity/TermScheduleActivity.java
@@ -1,9 +1,11 @@
 package by.toggi.rxbsuir.activity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 
 import java.util.List;
 
@@ -13,24 +15,29 @@ import butterknife.Bind;
 import by.toggi.rxbsuir.R;
 import by.toggi.rxbsuir.RxBsuirApplication;
 import by.toggi.rxbsuir.SubheaderItemDecoration;
-import by.toggi.rxbsuir.adapter.LessonAdapter;
+import by.toggi.rxbsuir.adapter.LessonWithDateAdapter;
 import by.toggi.rxbsuir.component.DaggerTermScheduleActivityComponent;
 import by.toggi.rxbsuir.db.model.LessonWithDate;
 import by.toggi.rxbsuir.module.TermScheduleActivityModule;
+import by.toggi.rxbsuir.mvp.presenter.TermPresenter;
 import by.toggi.rxbsuir.mvp.view.TermView;
 
-public class TermScheduleActivity extends ScheduleActivity implements TermView {
+public class TermScheduleActivity extends ScheduleActivity implements TermView, SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Bind(R.id.recycler_view) RecyclerView mRecyclerView;
 
     @Inject LinearLayoutManager mLayoutManager;
-    @Inject LessonAdapter mAdapter;
+    @Inject LessonWithDateAdapter mAdapter;
+    @Inject TermPresenter mPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setupRecyclerView();
+
+        mPresenter.attachView(this);
+        mPresenter.onCreate();
 
     }
 
@@ -49,7 +56,7 @@ public class TermScheduleActivity extends ScheduleActivity implements TermView {
 
     @Override
     protected void showToday() {
-
+        mRecyclerView.scrollToPosition(mAdapter.getTodayPosition());
     }
 
     private void setupRecyclerView() {
@@ -63,6 +70,36 @@ public class TermScheduleActivity extends ScheduleActivity implements TermView {
 
     @Override
     public void showLessonList(List<LessonWithDate> lessonWithDateList) {
+        mAdapter.setLessonWithDateList(lessonWithDateList);
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mSharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mSharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
+        boolean isGroupSchedule = preferences.getBoolean(ScheduleActivity.KEY_IS_GROUP_SCHEDULE, true);
+        switch (key) {
+            case ScheduleActivity.KEY_SYNC_ID:
+                mRecyclerView.setVisibility(View.GONE);
+                mPresenter.setSyncId(preferences.getString(key, null), isGroupSchedule);
+                break;
+            case ScheduleActivity.KEY_SUBGROUP_1:
+            case ScheduleActivity.KEY_SUBGROUP_2:
+                boolean subgroup1 = preferences.getBoolean(ScheduleActivity.KEY_SUBGROUP_1, true);
+                boolean subgroup2 = preferences.getBoolean(ScheduleActivity.KEY_SUBGROUP_2, true);
+                mPresenter.setSubgroupNumber(subgroup1, subgroup2, isGroupSchedule);
+                break;
+        }
     }
 }
