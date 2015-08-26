6a8bcdc521ae3da8228e555ef90a978fddc83cbe
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-08-26 20:46:17 +0300

    Remove unused methods in component, add SettingsFragment injection

--- app/src/main/java/by/toggi/rxbsuir/component/AppComponent.java
+++ app/src/main/java/by/toggi/rxbsuir/component/AppComponent.java
@@ -1,7 +1,5 @@
 package by.toggi.rxbsuir.component;
 
-import android.content.SharedPreferences;
-
 import com.f2prateek.rx.preferences.Preference;
 import com.pushtorefresh.storio.sqlite.StorIOSQLite;
 
@@ -15,13 +13,11 @@ import by.toggi.rxbsuir.activity.ScheduleActivity;
 import by.toggi.rxbsuir.activity.SettingsActivity;
 import by.toggi.rxbsuir.fragment.AddEmployeeDialogFragment;
 import by.toggi.rxbsuir.fragment.AddGroupDialogFragment;
+import by.toggi.rxbsuir.fragment.SettingsFragment;
 import by.toggi.rxbsuir.module.AppModule;
 import by.toggi.rxbsuir.module.BsuirServiceModule;
 import by.toggi.rxbsuir.module.DbModule;
 import by.toggi.rxbsuir.module.PreferencesModule;
-import by.toggi.rxbsuir.mvp.presenter.NavigationDrawerPresenter;
-import by.toggi.rxbsuir.mvp.presenter.SchedulePresenter;
-import by.toggi.rxbsuir.rest.BsuirService;
 import by.toggi.rxbsuir.service.LessonReminderService;
 import dagger.Component;
 
@@ -36,27 +32,13 @@ public interface AppComponent {
 
     RxBsuirApplication app();
 
-    BsuirService bsuirService();
-
     StorIOSQLite storIOSQLite();
-
-    SharedPreferences sharedPreferences();
 
     @Named(PreferenceHelper.SYNC_ID) Preference<String> rxSyncId();
 
     @Named(PreferenceHelper.IS_GROUP_SCHEDULE) Preference<Boolean> rxIsGroupSchedule();
 
-    @Named(PreferenceHelper.TITLE) Preference<String> rxTitle();
-
-    Preference<Integer> rxItemId();
-
     Preference<SubgroupFilter> rxSubgroupFilter();
-
-    @Named(PreferenceHelper.IS_DARK_THEME) boolean isDarkTheme();
-
-    @Named(PreferenceHelper.FAVORITE_SYNC_ID) Preference<String> rxFavoriteSyncId();
-
-    @Named(PreferenceHelper.FAVORITE_IS_GROUP_SCHEDULE) Preference<Boolean> rxFavoriteIsGroupSchedule();
 
     void inject(AddGroupDialogFragment addGroupDialogFragment);
 
@@ -68,8 +50,6 @@ public interface AppComponent {
 
     void inject(LessonReminderService lessonReminderService);
 
-    SchedulePresenter schedulePresenter();
-
-    NavigationDrawerPresenter navigationDrawerPresenter();
+    void inject(SettingsFragment settingsFragment);
 
 }
