f489da8f03c5771d62e42db710c7705199657af0
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-08-26 20:46:51 +0300

    Add Preference<LocalTime> for storing notification time

--- app/src/main/java/by/toggi/rxbsuir/module/PreferencesModule.java
+++ app/src/main/java/by/toggi/rxbsuir/module/PreferencesModule.java
@@ -2,9 +2,12 @@ package by.toggi.rxbsuir.module;
 
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 
 import com.f2prateek.rx.preferences.Preference;
 import com.f2prateek.rx.preferences.RxSharedPreferences;
+
+import org.threeten.bp.LocalTime;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
@@ -86,6 +89,27 @@ public class PreferencesModule {
     @Named(PreferenceHelper.FAVORITE_IS_GROUP_SCHEDULE)
     Preference<Boolean> provideRxFavoriteIsGroupSchedule(RxSharedPreferences preferences) {
         return preferences.getBoolean(PreferenceHelper.FAVORITE_IS_GROUP_SCHEDULE, true);
+    }
+
+    @Provides
+    @Singleton
+    Preference<LocalTime> provideRxNotificationTime(RxSharedPreferences preferences) {
+        return preferences.getObject(PreferenceHelper.NOTIFICATION_TIME, LocalTime.of(7, 0), LocalTimeAdapter.INSTANCE);
+    }
+
+    private static final class LocalTimeAdapter implements Preference.Adapter<LocalTime> {
+
+        static final LocalTimeAdapter INSTANCE = new LocalTimeAdapter();
+
+        @Override
+        public LocalTime get(@NonNull String key, @NonNull SharedPreferences preferences) {
+            return LocalTime.parse(preferences.getString(key, "07:00"));
+        }
+
+        @Override
+        public void set(@NonNull String key, @NonNull LocalTime localTime, @NonNull SharedPreferences.Editor editor) {
+            editor.putString(key, localTime.toString());
+        }
     }
 
 }
