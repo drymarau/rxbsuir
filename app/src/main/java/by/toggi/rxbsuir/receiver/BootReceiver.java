16e93bf375620c54fc3a8b97ef672e71568ff473
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-08-27 18:53:43 +0300

    Reset alarm on boot

---
+++ app/src/main/java/by/toggi/rxbsuir/receiver/BootReceiver.java
@@ -0,0 +1,29 @@
+package by.toggi.rxbsuir.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import org.threeten.bp.LocalTime;
+
+import by.toggi.rxbsuir.PreferenceHelper;
+import by.toggi.rxbsuir.Utils;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        scheduleAlarms(context);
+    }
+
+    private void scheduleAlarms(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String favoriteSyncId = preferences.getString(PreferenceHelper.FAVORITE_SYNC_ID, null);
+        if (favoriteSyncId != null) {
+            String localTimeString = preferences.getString(PreferenceHelper.NOTIFICATION_TIME, "07:00");
+            Utils.setAlarm(context, LocalTime.parse(localTimeString));
+        }
+    }
+}
