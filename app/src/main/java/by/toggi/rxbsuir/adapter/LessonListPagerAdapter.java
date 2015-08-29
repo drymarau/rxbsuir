d1de940a73cca86b707562568515e2d5d1a60452
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-08-29 20:44:38 +0300

    Rename PagerAdapter

--- app/src/main/java/by/toggi/rxbsuir/adapter/LessonListPagerAdapter.java
+++ app/src/main/java/by/toggi/rxbsuir/adapter/LessonListPagerAdapter.java
@@ -4,20 +4,30 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
+import by.toggi.rxbsuir.fragment.TodayFragment;
 import by.toggi.rxbsuir.fragment.WeekFragment;
 
-public class WeekPagerAdapter extends FragmentStatePagerAdapter {
+public class LessonListPagerAdapter extends FragmentStatePagerAdapter {
 
     private final String[] mTabs;
 
-    public WeekPagerAdapter(FragmentManager fm, String[] tabs) {
+    public LessonListPagerAdapter(FragmentManager fm, String[] tabs) {
         super(fm);
         mTabs = tabs;
     }
 
     @Override
     public Fragment getItem(int position) {
-        return WeekFragment.newInstance(position + 1);
+        if (getCount() == 4) {
+            // Only weeks are shown
+            return WeekFragment.newInstance(position + 1);
+        } else {
+            if (position == 0) {
+                // Today tab and weeks are shown
+                return TodayFragment.newInstance();
+            }
+        }
+        return WeekFragment.newInstance(position);
     }
 
     @Override
