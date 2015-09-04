d5de16666834b25672932749e4c00802900cfb96
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-09-04 18:42:53 +0300

    Add toolbar to lesson activity layout

--- app/src/main/java/by/toggi/rxbsuir/activity/LessonActivity.java
+++ app/src/main/java/by/toggi/rxbsuir/activity/LessonActivity.java
@@ -4,15 +4,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import org.parceler.Parcels;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import by.toggi.rxbsuir.R;
 import by.toggi.rxbsuir.db.model.Lesson;
 
 public class LessonActivity extends AppCompatActivity {
 
     private static final String EXTRA_LESSON = "by.toggi.rxbsuir.extra.lesson";
+
+    @Bind(R.id.toolbar) Toolbar mToolbar;
 
     public static void start(Context context, Lesson lesson) {
         Intent intent = new Intent(context, LessonActivity.class);
@@ -23,9 +29,20 @@ public class LessonActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson);
+
+        ButterKnife.bind(this);
+
+        setupToolbar();
 
         Lesson lesson = Parcels.unwrap(getIntent().getParcelableExtra(EXTRA_LESSON));
         Toast.makeText(this, lesson.getPrettyLesson(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(mToolbar);
+        getDelegate().getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mToolbar.setNavigationOnClickListener(v -> onBackPressed());
     }
 
 }
