ef4f7626943c36867c23d935c0863ce2e133c577
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-09-05 17:51:11 +0300

    Setup detail view

--- app/src/main/java/by/toggi/rxbsuir/activity/LessonActivity.java
+++ app/src/main/java/by/toggi/rxbsuir/activity/LessonActivity.java
@@ -3,22 +3,42 @@ package by.toggi.rxbsuir.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.widget.Toast;
 
 import org.parceler.Parcels;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import by.toggi.rxbsuir.PreferenceHelper;
 import by.toggi.rxbsuir.R;
+import by.toggi.rxbsuir.RxBsuirApplication;
+import by.toggi.rxbsuir.adapter.DetailItemAdapter;
 import by.toggi.rxbsuir.db.model.Lesson;
+import by.toggi.rxbsuir.mvp.presenter.LessonDetailPresenter;
+import by.toggi.rxbsuir.mvp.view.LessonDetailView;
 
-public class LessonActivity extends AppCompatActivity {
+public class LessonActivity extends AppCompatActivity implements LessonDetailView {
 
     private static final String EXTRA_LESSON = "by.toggi.rxbsuir.extra.lesson";
 
     @Bind(R.id.toolbar) Toolbar mToolbar;
+    @Bind(R.id.collapsing_toolbar) CollapsingToolbarLayout mCollapsingToolbarLayout;
+    @Bind(R.id.recycler_view) RecyclerView mRecyclerView;
+
+    @Inject @Named(PreferenceHelper.IS_DARK_THEME) boolean mIsDarkTheme;
+    @Inject LessonDetailPresenter mPresenter;
+
+    private DetailItemAdapter mAdapter;
 
     public static void start(Context context, Lesson lesson) {
         Intent intent = new Intent(context, LessonActivity.class);
@@ -28,6 +48,10 @@ public class LessonActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ((RxBsuirApplication) getApplication()).getAppComponent().inject(this);
+
+        setTheme(mIsDarkTheme ? R.style.AppTheme_Dark : R.style.AppTheme_Light);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson);
 
@@ -35,14 +59,77 @@ public class LessonActivity extends AppCompatActivity {
 
         setupToolbar();
 
+
         Lesson lesson = Parcels.unwrap(getIntent().getParcelableExtra(EXTRA_LESSON));
-        Toast.makeText(this, lesson.getPrettyLesson(), Toast.LENGTH_SHORT).show();
+
+        mCollapsingToolbarLayout.setTitle(lesson.getSubjectWithSubgroup());
+
+        mAdapter = new DetailItemAdapter(new ArrayList<>());
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mPresenter.attachView(this);
+        mPresenter.setLesson(lesson);
+    }
+
+    @Override
+    public void showLessonDetail(List<DetailItem> detailItemList) {
+        mAdapter.setDetailItemList(detailItemList);
     }
 
     private void setupToolbar() {
         setSupportActionBar(mToolbar);
         getDelegate().getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mToolbar.setNavigationOnClickListener(v -> onBackPressed());
+    }
+
+    public static class DetailItem {
+
+        private Type mType;
+        private String mText;
+        private String mSummary;
+        private boolean mIsIconVisible;
+
+        private DetailItem() {
+        }
+
+        public static DetailItem newInstance(Type type, String text, String summary, boolean isIconVisible) {
+            DetailItem detailItem = new DetailItem();
+            detailItem.mType = type;
+            detailItem.mText = text;
+            detailItem.mSummary = summary;
+            detailItem.mIsIconVisible = isIconVisible;
+            return detailItem;
+        }
+
+        public static DetailItem newInstance(Type type, String text, boolean isIconVisible) {
+            DetailItem detailItem = new DetailItem();
+            detailItem.mType = type;
+            detailItem.mText = text;
+            detailItem.mIsIconVisible = isIconVisible;
+            return detailItem;
+        }
+
+        public Type getType() {
+            return mType;
+        }
+
+        public String getText() {
+            return mText;
+        }
+
+        public String getSummary() {
+            return mSummary;
+        }
+
+        public boolean isIconVisible() {
+            return mIsIconVisible;
+        }
+
+        public enum Type {
+            TIME, EMPLOYEE, GROUP, AUDITORY, WEEKDAY, NOTE
+        }
+
     }
 
 }
---
+++ app/src/main/java/by/toggi/rxbsuir/adapter/DetailItemAdapter.java
@@ -0,0 +1,105 @@
+package by.toggi.rxbsuir.adapter;
+
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import by.toggi.rxbsuir.R;
+import by.toggi.rxbsuir.activity.LessonActivity.DetailItem;
+
+public class DetailItemAdapter extends RecyclerView.Adapter<DetailItemAdapter.ViewHolder> {
+
+    private static final int VIEW_TYPE_DETAIL = 0;
+    private static final int VIEW_TYPE_DETAIL_SUMMARY = 1;
+    private List<DetailItem> mDetailItemList;
+
+    public DetailItemAdapter(List<DetailItem> detailItemList) {
+        mDetailItemList = detailItemList;
+    }
+
+    public void setDetailItemList(List<DetailItem> detailItemList) {
+        mDetailItemList.clear();
+        mDetailItemList.addAll(detailItemList);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (mDetailItemList.get(position).getType() == DetailItem.Type.WEEKDAY) {
+            return VIEW_TYPE_DETAIL_SUMMARY;
+        }
+        return VIEW_TYPE_DETAIL;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        switch (viewType) {
+            case VIEW_TYPE_DETAIL:
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_detail, parent, false);
+                break;
+            case VIEW_TYPE_DETAIL_SUMMARY:
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_detail_summary, parent, false);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown viewType: " + viewType);
+        }
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        DetailItem detailItem = mDetailItemList.get(position);
+        holder.mIcon.setVisibility(detailItem.isIconVisible() ? View.VISIBLE : View.GONE);
+        switch (detailItem.getType()) {
+            case TIME:
+                holder.mIcon.setImageResource(R.drawable.ic_time);
+                break;
+            case EMPLOYEE:
+                holder.mIcon.setImageResource(R.drawable.ic_employee);
+                break;
+            case GROUP:
+                holder.mIcon.setImageResource(R.drawable.ic_group);
+                break;
+            case AUDITORY:
+                holder.mIcon.setImageResource(R.drawable.ic_auditory);
+                break;
+            case WEEKDAY:
+                holder.mIcon.setImageResource(R.drawable.ic_action_today);
+                break;
+            case NOTE:
+                holder.mIcon.setImageResource(R.drawable.ic_note);
+                break;
+        }
+        holder.mText.setText(detailItem.getText());
+        if (holder.mSummary != null) {
+            holder.mSummary.setText(detailItem.getSummary() == null ? "Еженедельно" : "Неделя " + detailItem.getSummary());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDetailItemList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.lesson_detail_icon) ImageView mIcon;
+        @Bind(R.id.lesson_detail_text) TextView mText;
+        @Nullable @Bind(R.id.lesson_detail_summary) TextView mSummary;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+}
