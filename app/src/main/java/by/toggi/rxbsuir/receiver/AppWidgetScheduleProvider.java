e676b344bb6bb2d829075ca25da569617a572716
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-09-08 20:07:48 +0300

    Add clickable icon to layout

--- app/src/main/java/by/toggi/rxbsuir/receiver/AppWidgetScheduleProvider.java
+++ app/src/main/java/by/toggi/rxbsuir/receiver/AppWidgetScheduleProvider.java
@@ -1,5 +1,6 @@
 package by.toggi.rxbsuir.receiver;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
@@ -8,6 +9,7 @@ import android.net.Uri;
 import android.widget.RemoteViews;
 
 import by.toggi.rxbsuir.R;
+import by.toggi.rxbsuir.activity.WeekScheduleActivity;
 import by.toggi.rxbsuir.service.AppWidgetScheduleService;
 
 public class AppWidgetScheduleProvider extends AppWidgetProvider {
@@ -21,9 +23,13 @@ public class AppWidgetScheduleProvider extends AppWidgetProvider {
             intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, id);
             intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
 
+            Intent startActivity = new Intent(context, WeekScheduleActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, startActivity, 0);
+
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.appwidget_schedule);
             remoteViews.setRemoteAdapter(R.id.list_view, intent);
             remoteViews.setEmptyView(R.id.list_view, R.id.empty_state);
+            remoteViews.setOnClickPendingIntent(R.id.icon, pendingIntent);
 
             appWidgetManager.updateAppWidget(id, remoteViews);
         }
