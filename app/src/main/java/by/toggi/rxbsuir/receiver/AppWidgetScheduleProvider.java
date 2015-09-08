586ed3baffe8ff7819ce89823b281ffcb1334ffe
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-09-08 18:42:29 +0300

    Add dummy app widget

---
+++ app/src/main/java/by/toggi/rxbsuir/receiver/AppWidgetScheduleProvider.java
@@ -0,0 +1,13 @@
+package by.toggi.rxbsuir.receiver;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class AppWidgetScheduleProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
