38b1eab1d2e0c5d220bda74ecc328881ec68c74c
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2015-09-09 08:04:43 +0300

    Add done and discard buttons to AppWidgetConfigActivity

--- app/src/main/java/by/toggi/rxbsuir/activity/AppWidgetConfigActivity.java
+++ app/src/main/java/by/toggi/rxbsuir/activity/AppWidgetConfigActivity.java
@@ -4,16 +4,29 @@ import android.appwidget.AppWidgetManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import by.toggi.rxbsuir.R;
 import by.toggi.rxbsuir.receiver.AppWidgetScheduleProvider;
 
 public class AppWidgetConfigActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar) Toolbar mToolbar;
 
     private int mAppWidgetId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appwidget_config);
+
+        ButterKnife.bind(this);
+
+        setupToolbar();
 
         setResult(RESULT_CANCELED);
 
@@ -28,14 +41,35 @@ public class AppWidgetConfigActivity extends AppCompatActivity {
         if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
             finish();
         }
+    }
 
-        Intent updateIntent = new Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE, null, this, AppWidgetScheduleProvider.class);
-        updateIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
-        sendBroadcast(updateIntent);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_appwidget_config_activity, menu);
+        return true;
+    }
 
-        Intent resultValue = new Intent();
-        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
-        setResult(RESULT_OK, resultValue);
-        finish();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_done:
+                Intent updateIntent = new Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE, null, this, AppWidgetScheduleProvider.class);
+                updateIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+                sendBroadcast(updateIntent);
+
+                Intent resultValue = new Intent();
+                resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+                setResult(RESULT_OK, resultValue);
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupToolbar() {
+        getDelegate().setSupportActionBar(mToolbar);
+        getDelegate().getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mToolbar.setNavigationIcon(R.drawable.ic_action_discard);
+        mToolbar.setNavigationOnClickListener(view -> finish());
     }
 }
