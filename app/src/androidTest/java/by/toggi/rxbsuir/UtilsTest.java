73b63318b4c3aec1f93253f928f466fe355dce1f
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2017-09-08 23:10:39 +0300

    Move dependencies to a standalone file

--- app/src/androidTest/java/by/toggi/rxbsuir/UtilsTest.java
+++ app/src/androidTest/java/by/toggi/rxbsuir/UtilsTest.java
@@ -4,9 +4,7 @@ import android.app.Application;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
-
 import com.jakewharton.threetenabp.AndroidThreeTen;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,48 +14,33 @@ import org.threeten.bp.zone.ZoneRulesProvider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(AndroidJUnit4.class)
-public class UtilsTest extends ApplicationTestCase<Application> {
-    public UtilsTest() {
-        super(Application.class);
-    }
+@RunWith(AndroidJUnit4.class) public class UtilsTest extends ApplicationTestCase<Application> {
 
-    private final Application application =
-            (Application) InstrumentationRegistry.getTargetContext().getApplicationContext();
+  private final Application application;
 
-    @Before
-    public void setup() {
-        AndroidThreeTen.init(application);
-    }
+  public UtilsTest() {
+    super(Application.class);
+    application = (Application) InstrumentationRegistry.getTargetContext().getApplicationContext();
+  }
 
-    @Test
-    public void litmus() {
-        assertThat(ZoneRulesProvider.getAvailableZoneIds()).isNotEmpty();
-    }
+  @Before public void setup() {
+    AndroidThreeTen.init(application);
+  }
 
-    @Test
-    public void getWeekNumberTest() {
-        assertThat(Utils.getWeekNumber(LocalDate.of(2015, Month.SEPTEMBER, 1))).isEqualTo(1);
-        assertThat(Utils.getWeekNumber(LocalDate.of(2016, Month.SEPTEMBER, 1))).isEqualTo(1);
+  @Test public void litmus() {
+    assertThat(ZoneRulesProvider.getAvailableZoneIds()).isNotEmpty();
+  }
 
-        assertThat(Utils.getWeekNumber(LocalDate.of(2015, Month.SEPTEMBER, 6))).isEqualTo(1);
+  @Test public void getWeekNumberTest() {
+    assertThat(Utils.getWeekNumber(LocalDate.of(2017, Month.SEPTEMBER, 1))).isEqualTo(1);
+    assertThat(Utils.getWeekNumber(LocalDate.of(2018, Month.SEPTEMBER, 1))).isEqualTo(1);
 
-        assertThat(Utils.getWeekNumber(LocalDate.of(2015, Month.SEPTEMBER, 7))).isEqualTo(2);
-        assertThat(Utils.getWeekNumber(LocalDate.of(2015, Month.SEPTEMBER, 13))).isEqualTo(2);
+    assertThat(Utils.getWeekNumber(LocalDate.of(2017, Month.SEPTEMBER, 4))).isEqualTo(2);
 
-        assertThat(Utils.getWeekNumber(LocalDate.of(2015, Month.SEPTEMBER, 14))).isEqualTo(3);
-        assertThat(Utils.getWeekNumber(LocalDate.of(2015, Month.SEPTEMBER, 20))).isEqualTo(3);
+    assertThat(Utils.getWeekNumber(LocalDate.of(2017, Month.SEPTEMBER, 8))).isEqualTo(2);
+    assertThat(Utils.getWeekNumber(LocalDate.of(2017, Month.SEPTEMBER, 11))).isEqualTo(3);
 
-        assertThat(Utils.getWeekNumber(LocalDate.of(2015, Month.SEPTEMBER, 21))).isEqualTo(4);
-        assertThat(Utils.getWeekNumber(LocalDate.of(2015, Month.SEPTEMBER, 27))).isEqualTo(4);
-
-        assertThat(Utils.getWeekNumber(LocalDate.of(2015, Month.DECEMBER, 31))).isEqualTo(2);
-        assertThat(Utils.getWeekNumber(LocalDate.of(2016, Month.JANUARY, 1))).isEqualTo(2);
-
-        assertThat(Utils.getWeekNumber(LocalDate.of(2015, Month.DECEMBER, 31))).isEqualTo(2);
-        assertThat(Utils.getWeekNumber(LocalDate.of(2016, Month.JANUARY, 1))).isEqualTo(2);
-        assertThat(Utils.getWeekNumber(LocalDate.of(2016, Month.JANUARY, 3))).isEqualTo(2);
-
-        assertThat(Utils.getWeekNumber(LocalDate.of(2016, Month.JANUARY, 4))).isEqualTo(3);
-    }
+    assertThat(Utils.getWeekNumber(LocalDate.of(2017, Month.SEPTEMBER, 18))).isEqualTo(4);
+    assertThat(Utils.getWeekNumber(LocalDate.of(2017, Month.SEPTEMBER, 22))).isEqualTo(4);
+  }
 }
