be2dfc068db2cb60efb71a58b94eb714ba2e29be
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2017-09-08 23:51:56 +0300

    Migrate to support preferences

--- app/src/main/java/by/toggi/rxbsuir/activity/SettingsActivity.java
+++ app/src/main/java/by/toggi/rxbsuir/activity/SettingsActivity.java
@@ -3,39 +3,36 @@ package by.toggi.rxbsuir.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
 import butterknife.BindView;
-import javax.inject.Inject;
-import javax.inject.Named;
-
 import butterknife.ButterKnife;
 import by.toggi.rxbsuir.PreferenceHelper;
 import by.toggi.rxbsuir.R;
 import by.toggi.rxbsuir.RxBsuirApplication;
 import by.toggi.rxbsuir.fragment.SettingsFragment;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    @BindView(R.id.toolbar) Toolbar mToolbar;
+  @BindView(R.id.toolbar) Toolbar mToolbar;
 
-    @Inject @Named(PreferenceHelper.IS_DARK_THEME) boolean mIsDarkTheme;
+  @Inject @Named(PreferenceHelper.IS_DARK_THEME) boolean mIsDarkTheme;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        ((RxBsuirApplication) getApplication()).getAppComponent().inject(this);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    ((RxBsuirApplication) getApplication()).getAppComponent().inject(this);
 
-        setTheme(mIsDarkTheme ? R.style.AppTheme_Dark : R.style.AppTheme_Light);
+    setTheme(mIsDarkTheme ? R.style.AppThemePreferenceBaseDark : R.style.AppThemePreferenceBase);
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_settings);
 
-        ButterKnife.bind(this);
-        setSupportActionBar(mToolbar);
-        getDelegate().getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        mToolbar.setNavigationOnClickListener(v -> onBackPressed());
+    ButterKnife.bind(this);
+    setSupportActionBar(mToolbar);
+    getDelegate().getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    mToolbar.setNavigationOnClickListener(v -> onBackPressed());
 
-        getFragmentManager().beginTransaction()
-                .replace(R.id.container_fragment_settings, new SettingsFragment())
-                .commit();
-    }
+    getSupportFragmentManager().beginTransaction()
+        .replace(R.id.container_fragment_settings, new SettingsFragment())
+        .commit();
+  }
 }
--- app/src/main/java/by/toggi/rxbsuir/fragment/SettingsFragment.java
+++ app/src/main/java/by/toggi/rxbsuir/fragment/SettingsFragment.java
@@ -1,112 +1,95 @@
 package by.toggi.rxbsuir.fragment;
 
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceFragment;
-import android.support.annotation.NonNull;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
-import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
-
-import org.threeten.bp.LocalTime;
-
-import javax.inject.Inject;
-import javax.inject.Named;
-
+import android.support.annotation.Nullable;
+import android.support.v7.preference.Preference;
 import by.toggi.rxbsuir.BuildConfig;
 import by.toggi.rxbsuir.PreferenceHelper;
 import by.toggi.rxbsuir.R;
 import by.toggi.rxbsuir.RxBsuirApplication;
 import by.toggi.rxbsuir.Utils;
+import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompat;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.threeten.bp.LocalTime;
 import rx.Subscription;
 
-public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener, TimePickerDialog.OnTimeSetListener {
+public class SettingsFragment extends PreferenceFragmentCompat
+    implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener,
+    TimePickerDialog.OnTimeSetListener {
 
-    @Inject @Named(PreferenceHelper.IS_DARK_THEME) boolean mIsDarkTheme;
-    @Inject com.f2prateek.rx.preferences.Preference<LocalTime> mLocalTimePreference;
-    @Inject @Named(PreferenceHelper.FAVORITE_SYNC_ID) com.f2prateek.rx.preferences.Preference<String> mFavoriteSyncIdPrerefence;
+  @Inject @Named(PreferenceHelper.IS_DARK_THEME) boolean mIsDarkTheme;
+  @Inject com.f2prateek.rx.preferences.Preference<LocalTime> mLocalTimePreference;
+  @Inject @Named(PreferenceHelper.FAVORITE_SYNC_ID) com.f2prateek.rx.preferences.Preference<String>
+      mFavoriteSyncIdPrerefence;
 
-    private Subscription mSubscription;
-    private Preference mNotificationTimePreference;
+  private Subscription mSubscription;
+  private Preference mNotificationTimePreference;
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        mSubscription = mLocalTimePreference.asObservable()
-                .subscribe(localTime -> {
-                    if (mNotificationTimePreference != null) {
-                        mNotificationTimePreference.setSummary(localTime.toString());
-                    }
-                    if (mFavoriteSyncIdPrerefence.get() != null) {
-                        Utils.setNotificationAlarm(getActivity(), localTime);
-                    }
-                });
+  @Override
+  public void onCreatePreferencesFix(@Nullable Bundle savedInstanceState, String rootKey) {
+    ((RxBsuirApplication) getActivity().getApplication()).getAppComponent().inject(this);
+
+    setPreferencesFromResource(R.xml.preferences, rootKey);
+
+    findPreference(PreferenceHelper.IS_DARK_THEME).setOnPreferenceChangeListener(this);
+    findPreference("build_version").setSummary(BuildConfig.VERSION_NAME);
+    findPreference("rate_app").setOnPreferenceClickListener(this);
+    findPreference(PreferenceHelper.IS_TODAY_ENABLED).setOnPreferenceChangeListener(this);
+    findPreference(PreferenceHelper.ARE_CIRCLES_COLORED).setOnPreferenceChangeListener(this);
+    findPreference(PreferenceHelper.IS_FAM_ENABLED).setOnPreferenceChangeListener(this);
+    mNotificationTimePreference = findPreference("notification_time");
+    mNotificationTimePreference.setEnabled(mFavoriteSyncIdPrerefence.get() != null);
+    mNotificationTimePreference.setOnPreferenceClickListener(this);
+  }
+
+  @Override public void onResume() {
+    super.onResume();
+    mSubscription = mLocalTimePreference.asObservable().subscribe(localTime -> {
+      if (mNotificationTimePreference != null) {
+        mNotificationTimePreference.setSummary(localTime.toString());
+      }
+      if (mFavoriteSyncIdPrerefence.get() != null) {
+        Utils.setNotificationAlarm(getActivity(), localTime);
+      }
+    });
+  }
+
+  @Override public void onPause() {
+    super.onPause();
+    Utils.unsubscribe(mSubscription);
+  }
+
+  @Override public boolean onPreferenceChange(Preference preference, Object newValue) {
+    switch (preference.getKey()) {
+      case PreferenceHelper.IS_DARK_THEME:
+      case PreferenceHelper.IS_TODAY_ENABLED:
+      case PreferenceHelper.ARE_CIRCLES_COLORED:
+      case PreferenceHelper.IS_FAM_ENABLED:
+        Utils.restartApp(getActivity());
+        break;
     }
+    return true;
+  }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        Utils.unsubscribe(mSubscription);
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        ((RxBsuirApplication) getActivity().getApplication()).getAppComponent().inject(this);
-
-        super.onCreate(savedInstanceState);
-
-        addPreferencesFromResource(R.xml.preferences);
-
-        findPreference(PreferenceHelper.IS_DARK_THEME).setOnPreferenceChangeListener(this);
-        findPreference("build_version").setSummary(BuildConfig.VERSION_NAME);
-        findPreference("rate_app").setOnPreferenceClickListener(this);
-        findPreference(PreferenceHelper.IS_TODAY_ENABLED).setOnPreferenceChangeListener(this);
-        findPreference(PreferenceHelper.ARE_CIRCLES_COLORED).setOnPreferenceChangeListener(this);
-        findPreference(PreferenceHelper.IS_FAM_ENABLED).setOnPreferenceChangeListener(this);
-        mNotificationTimePreference = findPreference("notification_time");
-        mNotificationTimePreference.setEnabled(mFavoriteSyncIdPrerefence.get() != null);
-        mNotificationTimePreference.setOnPreferenceClickListener(this);
-    }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.preference_list_view, container, false);
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        switch (preference.getKey()) {
-            case PreferenceHelper.IS_DARK_THEME:
-            case PreferenceHelper.IS_TODAY_ENABLED:
-            case PreferenceHelper.ARE_CIRCLES_COLORED:
-            case PreferenceHelper.IS_FAM_ENABLED:
-                Utils.restartApp(getActivity());
-                break;
-        }
+  @Override public boolean onPreferenceClick(Preference preference) {
+    switch (preference.getKey()) {
+      case "rate_app":
+        Utils.openPlayStorePage(getActivity());
+        return true;
+      case "notification_time":
+        LocalTime localTime = mLocalTimePreference.get();
+        TimePickerDialog dialog =
+            TimePickerDialog.newInstance(this, localTime.getHour(), localTime.getMinute(), true);
+        dialog.setThemeDark(mIsDarkTheme);
+        dialog.show(getActivity().getFragmentManager(), "time_picker");
         return true;
     }
+    return false;
+  }
 
-    @Override
-    public boolean onPreferenceClick(Preference preference) {
-        switch (preference.getKey()) {
-            case "rate_app":
-                Utils.openPlayStorePage(getActivity());
-                return true;
-            case "notification_time":
-                LocalTime localTime = mLocalTimePreference.get();
-                TimePickerDialog dialog = TimePickerDialog.newInstance(this, localTime.getHour(), localTime.getMinute(), true);
-                dialog.setThemeDark(mIsDarkTheme);
-                dialog.show(getFragmentManager(), "time_picker");
-                return true;
-        }
-        return false;
-    }
-
-    @Override
-    public void onTimeSet(RadialPickerLayout radialPickerLayout, int hour, int minute) {
-        mLocalTimePreference.set(LocalTime.of(hour, minute));
-    }
+  @Override public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
+    mLocalTimePreference.set(LocalTime.of(hourOfDay, minute));
+  }
 }
