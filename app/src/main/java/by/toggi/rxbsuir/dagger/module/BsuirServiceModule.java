fc5e3bf380f4e917d203f13caf4f284bb89e3d67
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2017-09-16 14:00:59 +0300

    Enable logging on RestAdapter

--- app/src/main/java/by/toggi/rxbsuir/dagger/module/BsuirServiceModule.java
+++ app/src/main/java/by/toggi/rxbsuir/dagger/module/BsuirServiceModule.java
@@ -6,11 +6,16 @@ import dagger.Module;
 import dagger.Provides;
 import javax.inject.Named;
 import retrofit.RestAdapter;
+import timber.log.Timber;
 
 @Module public class BsuirServiceModule {
 
-  @Provides @PerApp RestAdapter provideRestAdapter(@Named("bsuir") String url) {
-    return new RestAdapter.Builder().setEndpoint(url).build();
+  @Provides @PerApp RestAdapter provideRestAdapter(@Named("bsuir") String url,
+      @Named("debug") boolean debug) {
+    return new RestAdapter.Builder().setEndpoint(url)
+        .setLog(message -> Timber.tag("Retrofit").d(message))
+        .setLogLevel(debug ? RestAdapter.LogLevel.FULL : RestAdapter.LogLevel.BASIC)
+        .build();
   }
 
   @Provides @PerApp BsuirService provideBsuirService(RestAdapter restAdapter) {
